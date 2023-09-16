d7d2b0f1f9a542b36657accac442c5e0fc2872fe
Alex Nicolas <alexoliveira604@gmail.com>
2023-09-16 18:23:50 -0300

    feat: enable client side get webassembly files

--- src/server.js
+++ src/server.js
@@ -4,13 +4,22 @@ const path = require('path')
 const app = express()
 const port = 3000
 
-app.get('/:path', (req, res) => {
-    const file = `${path.join(__dirname)}/pages/${req.params.path}/index.html`
+app.get('/', (req, res) => {
+    const file = `${path.join(__dirname)}/pages/index.html`
     if (!file) {
         console.log('File not found')
         res.sendStatus(404)
     }
-    res.sendFile()
+    res.sendFile(file)
+})
+
+app.get('/script/:path', (req, res) => {
+    const file = `${path.join(__dirname)}/pages/${req.params.path}`
+    if (!file) {
+        console.log('File not found')
+        res.sendStatus(404)
+    }
+    res.sendFile(file)
 })
 
 app.listen(port, () => {
